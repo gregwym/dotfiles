6f033f045973f750b0e5c6bcbc7ba872fa80d122
Greg Wang <gregwym@users.noreply.github.com>
2019-11-22 02:16:25 +0800

    调整安装顺序

--- setup-brew.sh
+++ setup-brew.sh
@@ -15,25 +15,25 @@ brew install tree
 brew install wget
 brew install yarn
 
+brew cask install v2rayu
 brew cask install alfred
-brew cask install dropbox
 brew cask install docker
 brew cask install google-chrome
 brew cask install visual-studio-code
 brew cask install iina
 brew cask install spectacle
 brew cask install flux
-brew cask install sourcetree
-brew cask install github
 brew cask install namechanger
 brew cask install textmate
 brew cask install steam
 brew cask install origin
-brew cask install adobe-acrobat-reader
 brew cask install beyond-compare
 brew cask install daisydisk
 brew cask install postman
 brew cask install sequel-pro
-brew cask install v2rayu
 brew cask install tableplus
+brew cask install github
+brew cask install sourcetree
+brew cask install adobe-acrobat-reader
 brew cask install microsoft-office
+brew cask install dropbox
