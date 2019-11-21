db123c96fcacef6132b253664396d94685d84ff2
Greg Wang <gregwym@users.noreply.github.com>
2019-11-22 00:31:14 +0800

    调整setup-brew脚本，增加各种工具。

--- setup-brew.sh
+++ setup-brew.sh
@@ -1,1 +1,39 @@
-brew install ack autojump git git-lfs hub node nvm pyenv pyenv-virtualenv rbenv tree wget yarn zsh
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install zsh
+brew install ack
+brew install autojump
+brew install git
+brew install git-lfs
+brew install hub
+brew install node
+brew install nvm
+brew install pyenv
+brew install pyenv-virtualenv
+brew install rbenv
+brew install tree
+brew install wget
+brew install yarn
+
+brew cask install alfred
+brew cask install dropbox
+brew cask install docker
+brew cask install google-chrome
+brew cask install visual-studio-code
+brew cask install iina
+brew cask install spectacle
+brew cask install flux
+brew cask install sourcetree
+brew cask install github
+brew cask install namechanger
+brew cask install textmate
+brew cask install steam
+brew cask install origin
+brew cask install adobe-acrobat-reader
+brew cask install beyond-compare
+brew cask install daisydisk
+brew cask install postman
+brew cask install sequel-pro
+brew cask install v2rayu
+brew cask install tableplus
+brew cask install microsoft-office
