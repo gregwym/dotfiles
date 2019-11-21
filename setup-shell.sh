edff39394ceb575929590a73d46a88dc6501f1d3
Greg Wang <gregwym@users.noreply.github.com>
2019-11-22 00:51:10 +0800

    Add git config user name and email.

--- setup-shell.sh
+++ setup-shell.sh
@@ -1,1 +1,4 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git config --global user.name "Greg Wang"
+git config --global user.email "gregwym@users.noreply.github.com"
